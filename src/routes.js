d8e01571c5f8bcb54885d3c3e996a0d59c60c042
ssedoud <s.sedoud@gmail.com>
2016-03-02 12:36:15 +0100

    fix wrong import

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import PriceMinister from './pages/PriceMinister';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
-import productListPage from './pages/productListPage';
+import productListPage from './pages/ProductListPage';
 
 const routes = (
     <Route path="/" component={ PriceMinister }>
