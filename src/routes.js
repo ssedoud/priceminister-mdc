dc23fecb5d06ec2bc9eb2e6b0104ae2d152f31d8
marionanfreville <marionanfreville@club-internet.fr>
2016-03-02 13:28:43 +0100

    correction nom de l'import ProductListPage dans routage

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import PriceMinister from './pages/PriceMinister';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
-import productListPage from './pages/ProductListPage';
+import ProductListPage from './pages/ProductListPage';
 
 const routes = (
     <Route path="/" component={ PriceMinister }>
