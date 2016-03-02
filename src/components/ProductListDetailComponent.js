253387491031f2372294cf9db95f98322f3848ab
ssedoud <s.sedoud@gmail.com>
2016-03-02 16:12:30 +0100

    product list displayed

--- src/components/ProductListDetailComponent.js
+++ src/components/ProductListDetailComponent.js
@@ -1,39 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router';
+import OfferSummaryComponent from './productList/OfferSummaryComponent'
+import ScoreComponent from './common/ScoreComponent'
 
 
 class ProductListDetailComponent extends React.Component {
 
    render() {
+
+     var productLink = "/product/" + this.props.product.id;
+
      return <div className="product-box">
                <div className="row">
                  <div className="col-md-3">
-                   <Link to="/product/543772">
-                     <img src="http://pmcdn.priceminister.com/photo/The-Division-1062158383_L.jpg" width = "170" heigth= "170"
-                       className="img-responsive center-block" alt="Responsive image"/>
+                   <Link to={productLink}>
+                     <img src={this.props.product.imagesUrls[0]} width = "170" heigth= "170" className="img-responsive center-block" alt="Responsive image"/>
                    </Link>
                  </div>
-                 <div className="col-md-5"> <a href = "#" className="title"> products</a> <br/>
-                   <span href = "#" className="glyphicon glyphicon-star yellow"> </span>
-                   <span href = "#" className="glyphicon glyphicon-star yellow"> </span>
-                   <span href = "#" className="glyphicon glyphicon-star yellow"> </span>
-                   <span href = "#" className="glyphicon glyphicon-star yellow"> </span>
-                   <span href = "#" className="glyphicon glyphicon-star yellow"> </span>
-                   <a href = "#" className = "avis"> (41 avis)</a> <br/><br/>
+
+                 <div className="col-md-5">
+                   <a href = "#" className="title"> {this.props.product.headline}</a>
+                   <br/>
+                   <ScoreComponent
+                   score={this.props.product.reviewsAverageNote}
+                   reviewCount={this.props.product.nbReviews}/>
                    <span className="selling" ><button className="btn btn-default btn-sm">Vendez le vôtre</button><br/><br/></span>
-                 </div>
-                 <div className="col-md-4">
-                   <a className="price" href="#" > 69,99 € </a>  Comme Neuf <br/>
-                   <a className="shipping" href="#"> + 2,90 € (frais de port) </a>
-                   <br/><br/>
-                   <button className="btn btn-default btn-sm bkColorPM"><span className="glyphicon glyphicon-shopping-cart"> </span> Ajouter au panier</button>
-                   <br/><br/>
-                   <span className="sellerSummary">Voir tous les vendeurs</span> <br/>
-                   <span className="offerSummary"> 21 neufs dès </span>
-                   <a href ="#"><span className="smallPrice">53,69 € </span> </a><br/>
-                   <span className="offerSummary">72 occasions dès </span>
-                   <a href ="#"><span className="smallPrice">51,99 € </span> </a>
-                 </div>
+                </div>
+
+                   <OfferSummaryComponent
+                   advertsUsedCount={this.props.product.advertsUsedCount}
+                   usedBestPrice={this.props.product.usedBestPrice}
+                   bestPrice={this.props.product.bestPrice}
+                   advertNewCount={this.props.product.advertNewCount}
+                   newBestPrice={this.props.product.newBestPrice}
+                   />
+
                </div>
              </div>;
           }
