9a5290a0923737788c6cbaccc6be4355c476d8d9
marionanfreville <marionanfreville@club-internet.fr>
2016-03-03 19:00:12 +0100

    fiche produit - finitions et corrections

--- src/components/productPage/ProductBuyBoxComponent.js
+++ src/components/productPage/ProductBuyBoxComponent.js
@@ -6,28 +6,45 @@ class ProductBuyBoxComponent extends React.Component {
       super(props);
   }
 
+  shipping(advert) {
+    if (advert.shippingAmount == 0) {
+      return <span className="free-shipping">Livraison Gratuite</span>;
+    }
+    return <span className="shippingMode">+ {advert.shippingAmount} € (frais de port) </span>;
+  }
+
+  getBuyBoxAdvert(bestOffers) {
+    if (typeof bestOffers.new !== 'undefined' && typeof bestOffers.used !== 'undefined') {
+      if (bestOffers.new.score > bestOffers.used.score) {
+        return this.props.product.bestOffers.new.adverts[0];
+      }
+      else {
+        return this.props.product.bestOffers.used.adverts[0];
+      }
+    }
+    else if (typeof bestOffers.new !== 'undefined') {
+      return bestOffers.new.adverts[0];
+    }
+    else if (typeof bestOffers.used !== 'undefined') {
+      return bestOffers.used.adverts[0];
+    }
+    return [];
+  }
+
   renderBestOffer() {
     console.log("renderBestOffer");
-    var buyBoxAdverts = [];
+    var advert = this.getBuyBoxAdvert(this.props.product.bestOffers);
 
-    if (typeof this.props.product.bestOffers.new !== 'undefined') {
-      buyBoxAdverts = this.props.product.bestOffers.new;
-    }
-    else if (typeof this.props.product.bestOffers.used !== 'undefined') {
-      buyBoxAdverts = this.props.product.bestOffers.used;
-    }
-
-    var advert = buyBoxAdverts.adverts[0];
     return <div className="marginTop_15">
         <span className="price"> {advert.salePrice} € <QualityComponent quality={advert.quality} /> </span>
         &nbsp;&nbsp;<span className="stock"><span className="glyphicon glyphicon-ok"></span>&nbsp;En stock (1)</span>
 
         <br />
-        <span className="free-shipping">Livraison Gratuite</span>
+        {this.shipping(advert)}
         <br /><br />
 
         <img src="img/rsp_icon.png" width="20" height="20" alt="rsp" />
-        <span className="rsp">70 Super Points</span> à cumuler
+        <span className="rsp">{Number((advert.salePrice).toFixed(1))} Super Points</span> à cumuler
 
         <br /><br />
         Commentaire du vendeur :
@@ -53,6 +70,24 @@ class ProductBuyBoxComponent extends React.Component {
         <a href ="#"><span className="smallPrice">{this.props.product.newBestPrice} € </span> </a>&nbsp;&nbsp;&nbsp;&nbsp;
         <span className="offerSummary">{this.props.product.nbUsedAdverts} occasions dès </span>
         <a href ="#"><span className="smallPrice">{this.props.product.usedBestPrice} € </span> </a>
+
+          <br /><br />
+          <div className="seller-box">
+            <div className="seller-summary-box">
+              <div className="col-md-2 vcenter">
+                <img src="img/avatar.png"
+                     width="40"
+                     height="40"
+                     className="img-responsive img-circle center-block"
+                     alt="Responsive image" />
+              </div>
+              <div className="col-md-4 vcenter nopadding">
+                <span className="center-block"><a href="#">{advert.seller.login}</a></span>
+                4,6/5 - 1234 Ventes
+              </div>
+            </div>
+          </div>
+
       </div>;
   }
 
